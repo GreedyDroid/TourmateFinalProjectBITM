d0dc31695235d04b751e22e7557f6352e6c09deb
Sayed <nuruddinsayeed@gmail.com>
2017-10-27 15:12:26 +0600

    Online base Costlist added Database Updated for cost list

--- app/src/main/java/com/example/sayed/tourmate/events/event_detail/EventDetail.java
+++ app/src/main/java/com/example/sayed/tourmate/events/event_detail/EventDetail.java
@@ -33,7 +33,7 @@ import java.util.ArrayList;
 
 public class EventDetail extends AppCompatActivity {
 
-    
+
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
     private FirebaseDatabase database;
